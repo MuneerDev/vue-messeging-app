a5eeabe3bd8c4451ba48895742d7a128c6cdc0c4
Muneerdev <muneerabdulsattar@gmail.com>
2023-05-08 04:26:51 +0300

    style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mt-5">
     <messegingApp :converations="converations"></messegingApp>
   </div>
 </template>
@@ -9,32 +9,104 @@ import messegingApp from "./components/messegingApp.vue";
 const converations = [
   {
     name: "Haider",
+    lastMessage:
+      "Yeah, I watched the new Marvel movie last weekend. It was pretty good!",
+    messeges: [
+      { messege: "hello friend", outgoing: false },
+      { messege: "Hey my friend!", outgoing: true },
+      { messege: "how are are doing", outgoing: false },
+      { messege: "doing pretty well!", outgoing: true },
+      { messege: "Have you seen any good movies lately?", outgoing: true },
+      {
+        messege:
+          "Yeah, I watched the new Marvel movie last weekend. It was pretty good!",
+        outgoing: false,
+      },
+      {
+        messege:
+          "That's cool. I haven't seen it yet. I've been too busy with work.",
+        outgoing: true,
+      },
+      {
+        messege: "Yeah, I know what you mean. It's been crazy at my job too.",
+        outgoing: false,
+      },
+    ],
+  },
+  {
+    name: "Azez",
+    lastMessage: "Perfect. I've been wanting to try that place out.",
+    messeges: [
+      { messege: "hey my dear friend", outgoing: false },
+      { messege: "Hey!", outgoing: true },
+      { messege: "what you been up to", outgoing: true },
+      { messege: "nothing much", outgoing: false },
+      {
+        messege: "Do you want to grab lunch sometime this week?",
+        outgoing: true,
+      },
+      {
+        messege: "Sure, that sounds great. How about Wednesday?",
+        outgoing: false,
+      },
+      {
+        messege:
+          "Wednesday works for me. How about we meet at the new Italian place downtown?",
+        outgoing: true,
+      },
+      {
+        messege: "Perfect. I've been wanting to try that place out.",
+        outgoing: false,
+      },
+    ],
+  },
+  {
+    name: "Muneer",
+    lastMessage: "Definitely. Let me send you a list of my favorites.",
+    messeges: [
+      { messege: "wassup", outgoing: true },
+      { messege: "wassup", outgoing: false },
+      { messege: "did you play that game!", outgoing: true },
+      { messege: "hell yah i did", outgoing: false },
+      { messege: "Which game did you play?", outgoing: false },
+      {
+        messege:
+          "I played the new racing game that just came out. It's really fun!",
+        outgoing: true,
+      },
+      {
+        messege:
+          "Oh, cool. I haven't played that one yet. I've been playing a lot of RPGs lately.",
+        outgoing: false,
+      },
+      {
+        messege:
+          "I've been meaning to try out more RPGs. Do you have any recommendations?",
+        outgoing: true,
+      },
+      {
+        messege: "Definitely. Let me send you a list of my favorites.",
+        outgoing: false,
+      },
+    ],
+  },
+  {
+    name: "Sara",
     lastMessage: "doing pretty well!",
     messeges: [
       { messege: "hello friend", outgoing: false },
       { messege: "Hey my friend!", outgoing: true },
       { messege: "how are are doing", outgoing: false },
       { messege: "doing pretty well!", outgoing: true },
-    ],
-  },
-  {
-    name: "Azez",
-    lastMessage: "nothing much",
-    messeges: [
-      { messege: "hey my dear friend", outgoing: false },
-      { messege: "Hey!", outgoing: true },
-      { messege: "what you been up to", outgoing: true },
-      { messege: "nothing much", outgoing: false },
-    ],
-  },
-  {
-    name: "Muneer",
-    lastMessage: "hell yah i did",
-    messeges: [
-      { messege: "wassup", outgoing: true },
-      { messege: "wassup", outgoing: false },
-      { messege: "did you play that game!", outgoing: true },
-      { messege: "hell yah i did", outgoing: false },
+      { messege: "Want to catch up over coffee sometime?", outgoing: true },
+      { messege: "Sure, that sounds good!", outgoing: false },
+      { messege: "How about this weekend?", outgoing: true },
+      { messege: "I'm free on Sunday afternoon.", outgoing: false },
+      {
+        messege: "Sounds good, let's meet at the cafe on Main Street at 2 pm.",
+        outgoing: true,
+      },
+      { messege: "Great, see you then!", outgoing: false },
     ],
   },
 ];
--- src/components/messegingApp.vue
+++ src/components/messegingApp.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="container mx-auto max-w-[90%] relative bg-gray-200 min-h-[90vh] rounded">
+    class="container mx-auto max-w-[90%] relative bg-gray-200 min-h-[85vh] rounded overflow-y-auto">
     <ul class="grid gap-5 rounded mt-5 p-5">
       <li
         class="hover:bg-indigo-300 rounded p-3 pl-5 cursor-pointer duration-300 flex items-center gap-8 border-b border-gray-300 last:shadow-none last:border-b-0"
@@ -10,13 +10,13 @@
         <img :src="link + index" alt="person" class="rounded-full w-20 h-20" />
         <div class="font-bold text-2xl">
           <p>{{ converation.name }}</p>
-          <p class="font-normal text-gray-500 text-lg">
+          <p class="font-normal text-gray-500 text-lg truncate w-[15ch]">
             {{ converation.lastMessage }}
           </p>
         </div>
       </li>
     </ul>
-    <div :class="`${styleMesseges} ${dropDownNoActive}`">
+    <div :class="`${styleMesseges} ${dropDownNoActive} overflow-y-auto`">
       <button
         @click="SelectConverstion"
         class="px-5 pt-3 pb-4 w-fit bg-indigo-500 rounded-lg text-white font-bold text-3xl">
